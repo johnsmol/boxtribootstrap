9b680c5e0b1d04a4591c66978157dff70fac543f
Giovanni Piccolo <piccolog93@gmail.com>
2022-05-24 21:18:14 +0200

    add auth0 to react frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import LoginButton from './LoginButton';
+import Profile from './Profile';
+import LogoutButton from './LogoutButton';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <LoginButton></LoginButton>
+        <Profile></Profile>
+        <LogoutButton></LogoutButton>
       </header>
     </div>
   );
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Auth0Provider } from "@auth0/auth0-react";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<Auth0Provider
+		domain="dev-6xmqy5r4.us.auth0.com"
+		clientId="hK36rdDHclqkE2mtllWVxcoYSWkzIOaM"
+		redirectUri="http://localhost:3000"
+    audience="https://dev-6xmqy5r4.us.auth0.com/api/v2/"
+    scope="read:current_user update:current_user_metadata"
+	>
+		<React.StrictMode>
+			<App />
+		</React.StrictMode>
+	</Auth0Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
